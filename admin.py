f2291629619701945d0f55871e4d21f7b43b65da
jaye0410 <jaye412@gmail.com>
2024-09-11 11:43:32 +0900

    ファイル名をハイフンで区切っていたため修正

--- admin.py
+++ admin.py
@@ -14,14 +14,18 @@ files = [
   "basic_info.php",
   "contact.php",
   "overview.php",
-  "how_to_register_wookieebot.php"
+  "how_to_register_wookieebot.php",
+  "useful_external_links.php"
 ]
 
+#<?xml version="1.0" encoding="UTF-8"?>
+#<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
 # <url>
 #   <loc>https://swgoh-chiptips.jp/</loc>
 #   <lastmod>2024-09-06T19:02:44+09:00</lastmod>
 # </url>
-xml: str = ""
+xml: str = '<?xml version="1.0" encoding="UTF-8"?>\n'
+xml = xml + '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n'
 
 url_prefix:str = "<url>\n"
 url_suffix:str = "</url>\n"
@@ -33,5 +37,7 @@ for file in files:
   xml = xml + url_prefix + "  <loc>https://swgoh-chiptips.jp/" + file + "</loc>\n"
   xml = xml + "  <lastmod>" + ymd[0:10] + "T" + ymd[11:19] + "+09:00</lastmod>\n"
   xml = xml + url_suffix
+
+xml = xml + "</urlset>"
 
 print(xml)
